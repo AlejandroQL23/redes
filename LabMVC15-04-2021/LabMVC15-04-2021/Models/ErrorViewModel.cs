9a4cee52c9f1a371cdc0d01b9fdc1884d02eb13b
AlejandroQL23 <ale23200@gmail.com>
2021-06-26 19:21:44 -0600

    Primer adelandto contagiaDOS

---
+++ LabMVC15-04-2021/LabMVC15-04-2021/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LabMVC15_04_2021.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

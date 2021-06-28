d77a05b3ca1e929cdb4c140d0b227a0a8872ed80
fabiola-torres <fabirato09@gmail.com>
2021-06-28 00:50:29 -0600

    actualizadas 4 de 7 funciones

--- proy/src/app/app.component.ts
+++ proy/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input} from '@angular/core';
 import { RestService } from './rest.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,48 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'appBootstrap';
+  games:any = [];
+  game : any;
+  @Input() gameData = {ownerGame:'', password:'', name:''};
+  @Input() playerData = {gameId:'', password:'', name:''};
+  closeResult: string;
 
-  @Input() gameData = { name:'', password:'', server:''};
+  constructor(public rest:RestService, private route: ActivatedRoute, private router: Router,  private modalService: NgbModal) { }
+
+
+
+  ngOnInit() {
+    this.getGames();
+    this.getRoundsByID();
+  }
+
+  getGames() {
+    this.games = [];
+    this.rest.getGameList().subscribe((data: {}) => {
+      console.log(data);
+      this.games = data;
+    });
+  }
+//-----------------------------------------
+createGame() {
+  this.rest.createGame(this.gameData.name, this.gameData).subscribe((result) => {
+   // this.router.navigate(['']);
+    console.log("si pasa por el post");
+  }, (err) => {
+    console.log(err);
+  });
+}
+//-----------------------------------------
+
+getRoundsByID() {
+  this.rest.getRoundListByID(this.playerData,this.playerData.gameId).subscribe((data: {}) => {
+    console.log(data);
+    this.game = data;
+  });
+}
+
 
   newGame() {
  
@@ -22,4 +61,17 @@ export class AppComponent {
   
   public isCollapsed = true;
   public isCollapsed2 = true;
+  refresh(): void { window.location.reload(); }
+
+
+//------------------------------------------------
+
+
+
+
+
+
+
+
+
 }
